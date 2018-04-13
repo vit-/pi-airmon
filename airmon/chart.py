f97d9608acb6cee0c030a722ef4f0376ea62f1b2
Slam <3lnc.slam@gmail.com>
2018-04-13 17:06:03 +0300

    Dashed line for prediction

--- airmon/chart.py
+++ airmon/chart.py
@@ -5,7 +5,7 @@ from matplotlib import pyplot
 
 def draw_png(data, predictions):
     pyplot.plot(data)
-    pyplot.plot(predictions, color='red')
+    pyplot.plot(predictions, linestyle='dashed')
     pyplot.xlabel('time')
     pyplot.ylabel('co2')
     pyplot.grid(True)
