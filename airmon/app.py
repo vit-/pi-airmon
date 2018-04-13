f9dcb312fa0808234252e53e0c15aa9414d03c7d
Vitalii Vokhmin <vitaliy.vokhmin@gmail.com>
2018-04-13 15:17:26 +0300

    bind db

--- airmon/app.py
+++ airmon/app.py
@@ -5,10 +5,7 @@ from datetime import datetime, timedelta
 
 import aiotg
 
-from airmon import const
-from airmon.forecast import predict
-from airmon.storage import get_co2_levels_series, get_or_create_channel, remove_channel, get_channels_id
-from airmon.chart import draw_png
+from airmon import const, forecast, storage, chart
 
 
 BOT_TOKEN = os.environ.get('TG_BOT_TOKEN')
@@ -22,13 +19,13 @@ bot = aiotg.Bot(api_token=BOT_TOKEN, name=BOT_NAME)
 
 @bot.command(r'/start')
 async def start(chat, match):
-    get_or_create_channel(chat.id)
+    storage.get_or_create_channel(chat.id)
     return await chat.reply('You\'ve been added to the notifications list')
 
 
 @bot.command(r'/stop')
 async def stop(chat, match):
-    remove_channel(chat.id)
+    storage.remove_channel(chat.id)
     return await chat.reply('You\'ve been removed from the notifications list')
 
 
@@ -44,8 +41,8 @@ async def fire_alerts(predictions, severity):
     _LAST_ALERT = time.time()
 
     lookback = datetime.utcnow() - timedelta(hours=const.display_lookback_hours)
-    data = get_co2_levels_series(lookback)
-    img = draw_png(data, predictions)
+    data = storage.get_co2_levels_series(lookback)
+    img = chart.draw_png(data, predictions)
 
     for chid in get_channels_id:
         chat = bot.channel(chid)
@@ -55,7 +52,7 @@ async def fire_alerts(predictions, severity):
 async def monitor():
 
     while True:
-        predictions = predict()
+        predictions = forecast.predict()
         val = predictions[-1]
         if val > const.co2_level_critical:
             await fire_alerts(predictions, 'CRITICAL')
@@ -65,6 +62,8 @@ async def monitor():
 
 
 if __name__ == '__main__':
+    print('Starting app')
+    storage.bind_db()
     loop = asyncio.get_event_loop()
     loop.call_soon(monitor)
     loop.run_until_complete(bot.loop())
